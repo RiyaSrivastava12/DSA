106c54770676bdd6bda308b6c5f8e8be15af1ace
RiyaSrivastava12 <srivastava.riya12@gmail.com>
2024-02-28 18:11:31 +0530

    added linearsearch

---
+++ ARRAY/LinearSearch.js
@@ -0,0 +1,15 @@
+
+function searchElement(arr,num){
+    for(let i =0; i<=arr.length-1;i++)
+    {
+        if(arr[i] == num)
+        {
+            return i;
+        } 
+    }
+    return -1
+}
+
+
+let arr= [1, 2, 3, 4, 5], num = 5;
+console.log(searchElement(arr,num));
