0daf9d497a23fe5636eb90e9a0aed2cb5eed49ec
RiyaSrivastava12 <srivastava.riya12@gmail.com>
2024-02-29 15:45:42 +0530

    added union of two array

--- ARRAY/UnionOfTwoArray.js
+++ ARRAY/UnionOfTwoArray.js
@@ -20,3 +20,13 @@ const arr2 = [2, 3, 4, 4, 5, 11, 12];
 
 const union = findUnion(arr1, arr2);
 console.log(union);
+
+/**
+ * Time Compleixty : O( (m+n)log(m+n) ) . Inserting an element in a set takes logN time, where N is no of elements in the set. At max set can store m+n elements {when there are no common elements and elements in arr,arr2 are distntict}. So Inserting m+n th element takes log(m+n) time. Upon approximation across inserting all elements in worst, it would take O((m+n)log(m+n) time.
+
+Using HashSet also takes the same time, On average insertion in unordered_set takes O(1) time but sorting the union vector takes O((m+n)log(m+n))  time. Because at max union vector can have m+n elements.
+
+Space Complexity : O(m+n) {If Space of Union ArrayList is considered} 
+
+O(1) {If Space of union ArrayList is not considered}
+ */
