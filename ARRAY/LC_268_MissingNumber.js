85915b2d7e35911542ae49238ca53e8a47473235
RiyaSrivastava12 <srivastava.riya12@gmail.com>
2024-02-29 16:34:19 +0530

    added another approach

--- ARRAY/LC_268_MissingNumber.js
+++ ARRAY/LC_268_MissingNumber.js
@@ -8,3 +8,27 @@ function missingNumber(arr){
 
 let arr1 = [0,3,1];
 console.log(missingNumber(arr1));
+
+/**
+ * Complexity
+Time complexity:
+O(n)
+
+Space complexity:
+O(1)
+
+
+Another ways:-
+var missingNumber = function (nums) {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    actualSum = actualSum + nums[i]
+  }
+  
+  return expectedSum-actualSum
+};
+
+ */
