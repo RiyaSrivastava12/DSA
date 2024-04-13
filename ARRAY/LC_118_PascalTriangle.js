ba5383136f85f750e0e22a14fe32d1bfaa61a8b5
RiyaSrivastava12 <srivastava.riya12@gmail.com>
2024-04-13 12:40:02 +0530

    added LC_118

--- ARRAY/LC_118_PascalTriangle.js
+++ ARRAY/LC_118_PascalTriangle.js
@@ -49,4 +49,12 @@ console.log(generate(numRows)); // [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ], [ 1, 3, 3, 1 
 Space Complexity: The space complexity is also O(n^2), because the function stores all rows of Pascal’s Triangle up to numRows 
 in the result array. Each row is an array of length i, where i is the row number, so the total amount of space used is proportional
  to the square of numRows.
+
+ dry run:
+ Assuming numRows = 5 and result = [[1]] (as the first row of Pascal’s Triangle is always [1]), here’s how it works:
+
+First Iteration (i = 1): prevRow is the first row, which is [1]. currentRow is initialized with [1]. The inner loop is skipped because j < i is not true (1 < 1 is false). Then 1 is pushed to currentRow, making it [1, 1]. This row is then pushed to result, making result = [[1], [1, 1]].
+Second Iteration (i = 2): prevRow is now the second row, which is [1, 1]. currentRow is again initialized with [1]. In the inner loop, j = 1 is less than i = 2, so prevRow[j - 1] + prevRow[j] which is 1 + 1 = 2 is pushed to currentRow, making it [1, 2]. After the inner loop, 1 is pushed to currentRow, making it [1, 2, 1]. This row is then pushed to result, making result = [[1], [1, 1], [1, 2, 1]].
+Third Iteration (i = 3): prevRow is now the third row, which is [1, 2, 1]. currentRow is initialized with [1]. In the inner loop, for j = 1 and j = 2, 2 and 1 are pushed to currentRow, making it [1, 3, 3]. After the inner loop, 1 is pushed to currentRow, making it [1, 3, 3, 1]. This row is then pushed to result, making result = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]].
+Fourth Iteration (i = 4): prevRow is now the fourth row, which is [1, 3, 3, 1]. currentRow is initialized with [1]. In the inner loop, for j = 1, j = 2, and j = 3, 4, 6, and 4 are pushed to currentRow, making it [1, 4, 6, 4]. After the inner loop, 1 is pushed to currentRow, making it [1, 4, 6, 4, 1]. This row is then pushed to result, making result = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]].
  */
